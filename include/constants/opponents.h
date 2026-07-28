ee28f3d24a7956f9b43c49f748a0b689681aa7fd
fisham33 <fisham33@gmail.com>
2026-07-28 22:53:32 +1000

    feat(saveblock): expand saveblocks via config, add new flags/vars/trainer space (#7)
    * increase saveblock capacity, make new flags & vars
    
    * add slightly more space for trainers
    
    * qol - add running shoes & fast text

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -860,12 +860,12 @@
 #define TRAINER_BRENDAN_PLACEHOLDER         853
 #define TRAINER_MAY_PLACEHOLDER             854
 
-// NOTE: Because each Trainer uses a flag to determine when they are defeated, there is only space for 9 additional trainers before trainer flag space overflows
+// NOTE: Because each Trainer uses a flag to determine when they are defeated, there is only space for 15 additional trainers before trainer flag space overflows
 //       More space can be made by shifting flags around in constants/flags.h or changing how trainer flags are handled
 //       MAX_TRAINERS_COUNT can be increased but will take up additional saveblock space
 
 #define TRAINERS_COUNT_EMERALD     855
-#define MAX_TRAINERS_COUNT_EMERALD 864
+#define MAX_TRAINERS_COUNT_EMERALD 870
 
 #if IS_FRLG
 #define TRAINERS_COUNT                      TRAINERS_COUNT_FRLG
