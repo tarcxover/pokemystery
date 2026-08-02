6ad8e08184cd830b20de2cd912fd8bf4d4874db3
Miriam <miriamlefae@gmail.com>
2026-08-02 19:59:10 +0530

    feat(naming): rename conan/ran to miles/franziska
    Edgeworth is too long for the wirless adapter

--- src/strings.c
+++ src/strings.c
@@ -21,6 +21,7 @@ const u8 gText_ExpandedPlaceholder_Brendan[] = _("BRENDAN");
 const u8 gText_ExpandedPlaceholder_May[] = _("MAY");
 const u8 gText_ExpandedPlaceholder_Red[] = _("RED");
 const u8 gText_ExpandedPlaceholder_Green[] = _("GREEN");
+const u8 gText_ExpandedPlaceholder_Franziska[] = _("Franziska");
 const u8 gText_EggNickname[POKEMON_NAME_LENGTH + 1] = _("EGG");
 const u8 gText_Pokemon[POKEMON_NAME_LENGTH + 1] = _("POKéMON");
 const u8 gText_Player[] = _("PLAYER"); // Unused
