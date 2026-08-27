4491c60dbe10b5863c7923bd114e7f1f53bf6f6e
Miriam <miriamlefae@gmail.com>
2026-08-27 23:21:56 +0530

    feat(music): add draculas fate midi from cvaos

--- src/debug.c
+++ src/debug.c
@@ -4979,7 +4979,8 @@ static void DebugAction_DestroyFollowerNPC(u8 taskId)
     X(MUS_RG_ENCOUNTER_DEOXYS)      \
     X(MUS_RG_TRAINER_TOWER)         \
     X(MUS_RG_SLOW_PALLET)           \
-    X(MUS_RG_TEACHY_TV_MENU)
+    X(MUS_RG_TEACHY_TV_MENU)        \
+    X(MUS_CVAOS_DRACULASFATE)
 
 #define SOUND_LIST_SE               \
     X(SE_USE_ITEM)                  \
