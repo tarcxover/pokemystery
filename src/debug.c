dc096246d8b6e36ddefaf6c8acbc580b63354a50
Miriam <miriamlefae@gmail.com>
2026-08-29 22:57:05 +0530

    feat(music): add mus trial from ace attorney

--- src/debug.c
+++ src/debug.c
@@ -4981,7 +4981,8 @@ static void DebugAction_DestroyFollowerNPC(u8 taskId)
     X(MUS_RG_SLOW_PALLET)           \
     X(MUS_RG_TEACHY_TV_MENU)        \
     X(MUS_CVAOS_DRACULASFATE)       \
-    X(MUS_LOGIC)
+    X(MUS_LOGIC)                    \
+    X(MUS_TRIAL)
 
 #define SOUND_LIST_SE               \
     X(SE_USE_ITEM)                  \
