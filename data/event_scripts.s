ac736a6aba3b0546608fb772f59ee5570c1caac3
fisham33 <fisham33@gmail.com>
2026-09-01 01:08:43 +1000

    gozo murder reconstruction

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1688,6 +1688,7 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/scripts/tarc3_common.inc"
 	.include "data/scripts/tarc3_accusation.inc"
 	.include "data/scripts/tarc3_accusation_evidence.inc"
+	.include "data/scripts/tarc3_gozo_reconstruction.inc"
 	.include "data/scripts/pc_transfer.inc"
 	.include "data/scripts/questionnaire.inc"
 	.include "data/scripts/abnormal_weather.inc"
