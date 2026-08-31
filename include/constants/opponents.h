d6fa412ec0985115792fbf8689734c6c0d54ac66
fisham33 <fisham33@gmail.com>
2026-08-31 14:25:25 +1000

    feat(scripting): revised pokemon integration to story & some fixes (#32)
    - remove pincurchin, toxicroak and dachsbun
    - add stoutland
    - add trainer battles for most of the characters
    - add extra evidence collection
    - update tilesets (pipe, bloody cloth, garland, breadcrumbs) for inside/outside ship
    - add pokemon interactions for cramorant (cloth) and stoutland (breadcrumbs)
    - fix metatile display issues

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -860,12 +860,16 @@
 #define TRAINER_BRENDAN_PLACEHOLDER         853
 #define TRAINER_MAY_PLACEHOLDER             854
 #define TRAINER_TARC3_ICHIRO                855
+#define TRAINER_TARC3_KITARO                856
+#define TRAINER_TARC3_MARIKO                857
+#define TRAINER_TARC3_JOJI                  858
+#define TRAINER_TARC3_TATSUO                859
 
 // NOTE: Because each Trainer uses a flag to determine when they are defeated, there is only space for 15 additional trainers before trainer flag space overflows
 //       More space can be made by shifting flags around in constants/flags.h or changing how trainer flags are handled
 //       MAX_TRAINERS_COUNT can be increased but will take up additional saveblock space
 
-#define TRAINERS_COUNT_EMERALD     856
+#define TRAINERS_COUNT_EMERALD     860
 #define MAX_TRAINERS_COUNT_EMERALD 870
 
 #if IS_FRLG
